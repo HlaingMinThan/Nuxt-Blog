ddfcf91ca6647c6637840475a9f640e185f32694
HlaingMinThan <hlaingminthan@92@gmail.com>
2023-08-27 12:20:26 +0630

    layouts and nav done

---
+++ layouts/default.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <nav class="bg-white p-5 flex justify-between items-center container mx-auto">
+            <h1 class="text-3xl">Logo</h1>
+            <ul class="flex space-x-10 text-lg">
+                <li>
+                    <NuxtLink to="/">Home</NuxtLink>
+                </li>
+                <li>
+                    <NuxtLink to="/about">About</NuxtLink>
+                </li>
+                <li>
+                    <NuxtLink to="/contact">Contact</NuxtLink>
+                </li>
+            </ul>
+        </nav>
+        <div class="bg-gray-100 h-screen w-full">
+            {{$slot}}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
