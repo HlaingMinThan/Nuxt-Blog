34f717e7924ebe2bda6422baef06031268ff8d53
HlaingMinThan <hlaingminthan@92@gmail.com>
2023-09-01 12:15:25 +0630

    show login user info

--- pages/my-info.vue
+++ pages/my-info.vue
@@ -1,13 +1,16 @@
 <template>
-  <h1>My Info</h1>
+  <div class="flex items-center flex-col" v-if="user">
+    <h1 class="text-2xl font-bold">My Info</h1>
+    <p>Name - {{user.name}}</p>
+    <p>email - {{user.email}}</p>
+  </div>
 </template>
 
-<script>
-export default {
-
-}
+<script setup>
+let {$apiFetch} = useNuxtApp();
+let user = ref(null);
+onMounted(async() => {
+    let response = await $apiFetch('/api/user');
+    user.value = response;
+})
 </script>
-
-<style>
-
-</style>
